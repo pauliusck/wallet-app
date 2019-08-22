fc9161f7a497080dd31e67f24b102997183eeb2d
Paulius Cikanavicius <paulius.cikanavicius@gmail.com>
2019-08-22 14:47:23 +0200

    Details: Create layout

--- src/app/home/details/details.component.ts
+++ src/app/home/details/details.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './details.component.html'
 })
 export class DetailsComponent {
+
+  performTransaction(): void {
+    // TODO: Implement performTransaction method
+  }
 }
