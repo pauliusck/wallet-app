c0eee7d375566491c1090b7e6f97faa3428ab901
Paulius Cikanavicius <paulius.cikanavicius@gmail.com>
2019-08-22 15:07:46 +0200

    Details: Create walletForm; Shared module: Include Form modules

--- src/app/home/details/details.component.ts
+++ src/app/home/details/details.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'wallet-details',
   templateUrl: './details.component.html'
 })
 export class DetailsComponent {
+  walletForm: FormGroup = this.fb.group({
+    amount: [null, [Validators.required, Validators.nullValidator]]
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   performTransaction(): void {
     // TODO: Implement performTransaction method
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LocalStorageService} from '../model/local-storage.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     LocalStorageService
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
