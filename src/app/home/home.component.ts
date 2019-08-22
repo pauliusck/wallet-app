0a23b690b4ba19a196a0a1b129ee7fa4885a1cd2
Paulius Cikanavicius <paulius.cikanavicius@gmail.com>
2019-08-22 13:47:09 +0200

    Create Home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wallet-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
