dda4f6d710a5f87996c1f0279e780e178c1ce70a
Paulius Cikanavicius <paulius.cikanavicius@gmail.com>
2019-08-22 15:13:48 +0200

    Details: Implement performTransaction method

--- src/app/home/details/details.component.ts
+++ src/app/home/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TransactionService} from '../transaction.service';
 
 @Component({
   selector: 'wallet-details',
@@ -9,10 +10,25 @@ export class DetailsComponent {
   walletForm: FormGroup = this.fb.group({
     amount: [null, [Validators.required, Validators.nullValidator]]
   });
+  totalAmountError: boolean = false;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,
+              private transactionService: TransactionService) {}
 
   performTransaction(): void {
-    // TODO: Implement performTransaction method
+    if (this.walletForm.valid && this.walletForm.value.amount !== 0) {
+      const value = Number(this.walletForm.value.amount.toFixed(2)); // Trim value to 2 decimal points
+      if (this.transactionService.getTotalValue() + value >= 0) {
+        this.totalAmountError = false;
+        const transaction = {
+          date: new Date(Date.now()).toString(),
+          amount: this.walletForm.value.amount
+        };
+        this.transactionService.storeTransaction(transaction);
+        this.walletForm.reset({amount: null});
+      } else {
+        this.totalAmountError = true;
+      }
+    }
   }
 }
