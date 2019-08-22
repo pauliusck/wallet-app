a4269bd574c9de1d9a8ec85639928746e80a8362
Paulius Cikanavicius <paulius.cikanavicius@gmail.com>
2019-08-22 16:33:50 +0200

    Add ngx-order-pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LocalStorageService} from '../model/local-storage.service';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {OrderModule} from 'ngx-order-pipe';
 
 
 
@@ -11,13 +12,15 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    OrderModule
   ],
   providers: [
     LocalStorageService
   ],
   exports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OrderModule
   ]
 })
 export class SharedModule { }
